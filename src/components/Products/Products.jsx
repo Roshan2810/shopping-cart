15bd66da690ec9d9a9b364c9c0044421ea83be36
Roshan Shah <rosu285shah@gmail.com>
2021-01-31 19:52:37 +0530

    Products page done

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,4 +1,4 @@
-import { Container, Grid, Card, CardContent, CardActions, CardMedia, Typography, Button, CardHeader } from '@material-ui/core';
+import { Container, Grid, Card, CardContent, CardActions, CardMedia, Typography, Button, CardHeader, Paper } from '@material-ui/core';
 import React from 'react';
 import Header from '../common/Header';
 import Footer from '../common/Footer';
@@ -8,31 +8,102 @@ const data = [
     {
         id: 1,
         title: "Fresh Kiwi - Green, 3pcs",
-        image: "https://lh3.googleusercontent.com/proxy/uJM3pvC_4_NmMqOUFl6Xn-VNv-P4Z32Bh0VY01qmCEbYADgmULIJGYK42hfyNlO1q8ugZjmCPiamSTln8OwLotR2seLZQf9PJ_lpWMY",
+        image: "https://sukanfood.com/wp-content/uploads/2019/07/Kiwi-300x300.jpg",
         description: "Kiwi is oval shaped with a brownish outer skin. The flesh is bright green and juicy with tiny, edible black seeds.",
         price: "87",
     },
     {
         id: 2,
-        title: "Apple - Washington, Regular, 4pcs",
-        image: "https://images-na.ssl-images-amazon.com/images/I/81sXUXFQ2oL._SX466_.jpg",
-        description: "The bright red coloured and heart shaped Washington apple are crunchy, juicy and slightly sweet. Washington Apples are a natural.",
+        title: "Apple - Regular, 4pcs",
+        image: "https://www.bigbasket.com/media/uploads/p/m/40033819-2_6-fresho-apple-shimla.jpg",
+        description: "The bright red coloured and heart shaped Washington apple are crunchy, juicy and slightly sweet.",
         price: "187",
     },
     {
         id: 3,
-        title: "Fesho Pomegrante Peeled, 500gm",
-        image: "https://images-na.ssl-images-amazon.com/images/I/81sXUXFQ2oL._SX466_.jpg",
-        description: "Pomegrante variety has a glossy, vibrant and soft ruby-red skin. The inside of the fruit is divided into compartments by thin white.",
+        title: "Pomegrante Peeled, 500gm",
+        image: "https://www.bigbasket.com/media/uploads/p/m/40005823_7-fresho-pomegranate-peeled.jpg",
+        description: "Pomegrante variety has a glossy, vibrant and soft ruby-red skin. The inside of the fruit is divided into compartments .",
         price: "88",
     },
     {
         id: 4,
         title: "Capsicum green - 1 kg",
-        image: "https://5.imimg.com/data5/PB/NY/GLADMIN-23937840/fresho-pomegranate-peeled-500x500.png",
+        image: "https://publiccanteen.com/wp-content/uploads/2020/04/green-capsicum-organic-vegetable-500x500-1-1.jpg",
+        description: "Leaving a moderately pungent taste on the tongue. Green capsicums, also known as green peppers are bell shaped.",
+        price: "137",
+    },
+    {
+        id: 5,
+        title: "Tomato - Local, 500gm",
+        image: "https://www.opagrologica.it/wp-content/uploads/2018/01/tondoliscio-300x300.jpg",
+        description: "Fresho bring to you an exquisite range of locally grown organic tomatoes, which are available at bigbasket. These tomatoes",
+        price: "12",
+    },
+    {
+        id: 6,
+        title: "White - Bread, 400gm",
+        image: "https://www.bigbasket.com/media/uploads/p/m/40087525_2-fresho-sandwich-bread-safe-preservative-free.jpg",
+        description: "Freshly Baked bread is one of lifes greatest simple pleasure at Bigbasket we decided to give you just that.",
+        price: "32",
+    },
+    {
+        id: 7,
+        title: "Amul Paneer - 250gm",
+        image: "https://www.bigbasket.com/media/uploads/p/m/40096747_5-amul-fresh-paneer.jpg",
+        description: "Freshly Baked bread is one of lifes greatest simple pleasure at Bigbasket we decided to give you just that.",
+        price: "98",
+    },
+    {
+        id: 8,
+        title: "Epigamia Greek Yoghurt- 90gm",
+        image: "https://www.bigbasket.com/media/uploads/p/m/40075139_3-epigamia-greek-yogurt-blueberry.jpg",
+        description: "Epigamia means in Ancient Greek meant a way to Formalize the relationship between different nations. Epigma greek yoghurt",
+        price: "40",
+    },
+    {
+        id: 9,
+        title: "Epigamia Greek Yoghurt- 90gm",
+        image: "https://www.bigbasket.com/media/uploads/p/m/1201999_2-epigamia-greek-yogurt-strawberry.jpg",
+        description: "Leaving a moderately pungent taste on the tongue. Green capsicums, also known as green peppers are bell shaped.",
+        price: "40",
+    },
+    {
+        id: 10,
+        title: "Tata Tea Gold Leaf, 500gm",
+        image: "https://m.media-amazon.com/images/S/aplus-media/sota/19f2a663-5992-4c43-bede-6b6965657906._CR0,0,300,300_PT0_SX300__.jpg",
+        description: "Fresho bring to you an exquisite range of locally grown organic tomatoes, which are available at bigbasket. These tomatoes",
+        price: "165",
+    },
+    {
+        id: 11,
+        title: "Red Label Tea - 500gm",
+        image: "https://www.hpsss.in/wp-content/uploads/2020/10/15-300x300.jpg",
+        description: "Freshly Baked bread is one of lifes greatest simple pleasure at Bigbasket we decided to give you just that.",
+        price: "205",
+    },
+    {
+        id: 12,
+        title: "Bournvita- 750gm",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRrxTRyEwH4cndTWD3Pm1-U3jVdoTO3pyOwjA&usqp=CAU",
+        description: "Cadbury Bournvita is a delicious chocolate health drink which is enriched with Vitamin(D,B2,B9,B12). It combines",
+        price: "572",
+    },
+    {
+        id: 13,
+        title: "Listerine - 250ml",
+        image: "https://www.bigbasket.com/media/uploads/p/m/1208096_4-listerine-mouthwash-cool-mint.jpg",
+        description: "Listerine Cool mint Antibacterial Mouthwash rapid & easy to use, a 30 second 'slosh' with Listerine after",
+        price: "109",
+    },
+    {
+        id: 14,
+        title: "Johnson & Johnson",
+        image: "https://images-eu.ssl-images-amazon.com/images/I/31CDDcTVReL._SX300_SY300_QL70_ML2_.jpg",
         description: "Leaving a moderately pungent taste on the tongue. Green capsicums, also known as green peppers are bell shaped.",
         price: "137",
     }
+
 ]
 
 const processMenuList = () => {
@@ -49,7 +120,7 @@ const processMenuList = () => {
     }
     return <Grid item xs={12} style={{
         width: '100%',
-        height: "100%",
+        height: window.innerHeight + window.outerHeight,
         backgroundColor: '#e7e7ec',
         top: '0'
     }}>
@@ -82,9 +153,9 @@ const processMenuList = () => {
 }
 const processItemGrid = () => {
     return (
-        <Container>
-            <Grid container style={{ height: window.innerHeight }} spacing={1}>
-                <Grid item xs={3}>
+        <Container style={{ height: "100%" }}>
+            <Grid container style={{ height: "100%" }} spacing={1}>
+                <Grid item xs={3} style={{ height: '100%' }}>
                     {processMenuList()}
                 </Grid>
                 <Grid item xs={9}>
@@ -99,17 +170,11 @@ const processItemGrid = () => {
 
 const processProductCard = (value) => {
     return (
-        <Card key={value.id} style={{ borderBottom: "dashed ", boxShadow: 'none', borderRadius: '0%' }}>
+        <Card key={value.id} style={{ borderBottom: "dashed #e7e7ec", boxShadow: 'none', borderRadius: '0%' }}>
             <CardHeader
                 title={value.title}
             />
-            <CardMedia
-                style={{
-                    height: 0,
-                    paddingTop: '56.25%',
-                }}
-                image='/src/logo512.png'
-            />
+            <img width="90%" src={value.image} alt={value.description} />
             <CardContent>
                 <Typography variant="body2" color="textSecondary" component="p">
                     {value.description}
@@ -119,7 +184,7 @@ const processProductCard = (value) => {
                 <Typography variant="subtitle2">
                     {`MRP Rs.${value.price}`}
                 </Typography>
-                <Button style={{ textTransform: 'none' }} variant="contained" color="secondary">
+                <Button style={{ textTransform: 'none', borderRadius:'0%' }} variant="contained" color="secondary">
                     Buy Now
                 </Button>
             </CardActions>
@@ -141,11 +206,17 @@ const processProductLists = () => {
 }
 const Products = () => {
     return (
-        <>
-            <Header />
-            {processItemGrid()}
-            <Footer />
-        </>
+        <Grid container>
+            <Grid item xs={12}>
+                <Header />
+            </Grid>
+            <Grid item xs={12}>
+                {processItemGrid()}
+            </Grid>
+            <Grid item xs={12}>
+                <Footer />
+            </Grid>
+        </Grid>
     )
 }
 export default Products;
